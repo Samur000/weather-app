732d760d42ca4766e4a49443285734c5618dc7ac
Samur <107584285+Samur000@users.noreply.github.com>
2023-03-30 23:44:21 +0300

    Создал отделтные функции

--- main.js
+++ main.js
@@ -5,61 +5,73 @@ const form = document.querySelector('.form');
 const inputCity = form.querySelector('input');
 const header = document.querySelector('.header')
 
+function removeCard() {
+   const prevCard = document.querySelector('.card')
+   if (prevCard) prevCard.remove()
+}
+function showError(errorMessage) {
+
+   html = `<div class="error-block parent-block">
+               <div class="error-title">${errorMessage}</div>
+               <div class="close-btn" onclick="removeErrorBlock(this)">&times;</div>
+            </div>`
+
+   //* Выводим карточку при ошибке 
+   header.insertAdjacentHTML('afterend', html)
+
+   setTimeout(() => {
+      //* Удаляем карточку через 2 секунды
+      const errorBlock = document.querySelector('.error-block')
+      errorBlock.remove()
+   }, 3000);
+}
 function removeErrorBlock(btn) {
    btn.closest('.parent-block').remove()
 }
+function showCard({name, country, temp, condition}) {
+   html = `<div class="card">
+            <div class="card__title">${name} <span>${country}</span></div>
+            <div class="card__body">
+               <div class="card__weather">${temp}°с</div>
+               <div class="card__img"><img src="img/8.png" alt=""></div>
+            </div>
+            <div class="card-desc">${condition}</div>
+         </div>`
 
-form.addEventListener('submit', (e) => {
+   header.insertAdjacentHTML('afterend', html)
+}
+async function getWeather(city) {
+   const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+   const response = await fetch(url)
+   const data = await response.json()
+   return data
+}
+
+
+form.addEventListener('submit', async function (e) {
 
    //  Отмена отпраки формы
    e.preventDefault()
-
    // Получаем значение из инпута
    let city = inputCity.value.trim()
-   console.log(city);
+   // Получаем данные с сервера 
+   const data = await getWeather(city)
 
-   // Адрес запроса
-   const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+   if (data.error) {
+      //! Еслм есть ошибка - выводим ошибку
+      removeCard()
+      showError(data.error.message)
+   } else {
+      //* Еслм нет ошибки выодим карточку
+      removeCard()
 
-   fetch(url)
-   .then(res => res.json())
-   .then(data => {
-
-      if (data.error) {
-         const prevCard = document.querySelector('.card')
-         if (prevCard) prevCard.remove()
-         
-         html = `<div class="error-block parent-block">
-                     <div class="error-title">Такого города нет</div>
-                     <div class="close-btn" onclick="removeErrorBlock(this)">&times;</div>
-                  </div>`
-            header.insertAdjacentHTML('afterend', html)
-         setTimeout(() => {
-            const errorBlock = document.querySelector('.error-block')
-            errorBlock.remove()
-         }, 2000);
-      } else {
-         const prevCard = document.querySelector('.card')
-         if (prevCard) prevCard.remove()
-
-         html =  `<div class="card">
-                     <div class="card__title">${data.location.name} <span>${data.location.country}</span></div>
-               
-                     <div class="card__body">
-                        <div class="card__weather">${data.current.temp_c}°с</div>
-                        <div class="card__img"><img src="img/8.png" alt=""></div>
-                     </div>
-                     <div class="card-desc">${data.current.condition.text}</div>
-                  </div>`
-
-         header.insertAdjacentHTML('afterend', html)
+      const weatherData = {
+         name: data.location.name,
+         country: data.location.country,
+         temp: data.current.temp_c,
+         condition: data.current.condition.text
       }
 
-
-         // console.log(data);
-         // console.log(data.location.name);
-         // console.log(data.location.country);
-         // console.log(data.current.temp_c);
-         // console.log(data.current.condition.text);
-      })
+      showCard(weatherData)
+   }
 })
